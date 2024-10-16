a7f277dbf9d5ff226e331918645327897bebe9be
iqbalsonata <iqbalsonata2@gmail.com>
2024-10-16 10:49:30 +0700

    solved roman to integer problem

---
+++ src/roman_to_integer.zig
@@ -0,0 +1,95 @@
+// Example 1:
+// Input: s = "III"
+// Output: 3
+// Explanation: III = 3.
+//
+
+// Example 2:
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+//
+// Example 3:
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+//
+//  1000 > 0
+//  1000
+//  1000 > 100
+//  1100
+//  100 > 1000
+//  1100 - (100 * 2) + 1000
+//  1900
+//  1000 > 10
+//  1910
+//  10 > 100
+//  1910 - (10 * 2) + 100
+//  1990
+//  100 > 1
+//  1991
+//  1 > 5
+//  1991 - (1 * 2) + 5
+//  1889 + 5
+//  1994
+//
+//
+//
+// s only contain characters ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+
+const std = @import("std");
+const expect = std.testing.expect;
+const allocator = std.testing.allocator;
+
+fn romanToInt(s: []const u8) !i32 {
+    const romans = [7]u8{ 'I', 'V', 'X', 'L', 'C', 'D', 'M' };
+    const ints = [7]i32{ 1, 5, 10, 50, 100, 500, 1000 };
+
+    var map = std.AutoHashMap(u8, i32).init(allocator);
+    defer map.deinit();
+
+    for (romans, ints) |roman, int| {
+        map.put(roman, int) catch |e| switch (e) {
+            error.OutOfMemory => return error.OutOfMemory,
+            else => return e,
+        };
+    }
+
+    var result: i32 = 0;
+    var prev: i32 = if (map.contains(s[0])) map.get(s[0]).? else 0;
+
+    for (s) |c| {
+        if (map.contains(c)) {
+            result = if (prev >= map.get(c).?) result + map.get(c).? else result - (prev * 2) + map.get(c).?;
+        }
+        prev = map.get(c).?;
+    }
+
+    return result;
+}
+
+const TestCase = struct {
+    input: []const u8,
+    output: i32,
+};
+
+test romanToInt {
+    const test_case = [_]TestCase{ .{
+        .input = "III",
+        .output = 3,
+    }, .{
+        .input = "LVIII",
+        .output = 58,
+    }, .{
+        .input = "MCMXCIV",
+        .output = 1994,
+    }, .{
+        .input = "MMMDCCXXIV",
+        .output = 3724,
+    } };
+
+    for (test_case) |tc| {
+        const res = try romanToInt(tc.input);
+        try expect(res == tc.output);
+    }
+}
